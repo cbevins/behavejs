bf130c144d70837b3cbd28c7ec4846980c89a9ca
Collin D Bevins <cbevins@montana.com>
2025-08-10 11:43:53 -0600

    Added more modules

--- src/lib/modules/Keys.js
+++ src/lib/modules/Keys.js
@@ -4,9 +4,48 @@ export class Keys {
     static mcfg = Keys.meta+'config/'
     static mver = Keys.meta+'version'
 
-    // Generic
+    // Fuel Module properties
+    static dens = 'fiber density'
+    static depth = 'depth'
+    static efol = 'effective fuel/ovendry load'
+    static efmc = 'effective fuel/moisture content'
+    static efwl = 'effective fuel/water load'
+    static ehn  = 'effective heating number'
+    static heat = 'heat of combustion'
+    static load = 'ovendry load'
+    static mext = 'extinction moisture content'
     static mois = 'moisture content'
+    static net  = 'net ovendry load'
+    static sa   = 'surface area'
+    static savr = 'surface area to volume ratio'
+    static sawf = 'surface area weighting factor'
+    static scwf = 'size class weighting factor'
+    static scar = 'size class weighting factor array'
+    static seff = 'effective mineral content'
+    static size = 'size class'
+    static stot = 'total mineral content'
+    static vol  = 'volume'
 
+    // Standard fuel model properties
+    static fmalias    = 'alias'
+    static fmkey      = 'key'
+    static fmnumb     = 'number'
+    static fmcode     = 'code'
+    static fmlabel    = 'label'
+    static fmcured    = 'cured herb fraction'
+    static fmdepth    = 'depth'
+    static fmmext     = 'extinction moisture content'
+    static fmheatdead = 'dead/'+Keys.heat
+    static fmheatlive = 'live/'+Keys.heat
+    static fmload1    = 'dead/1-h/'+Keys.load
+    static fmload10   = 'dead/10-h/'+Keys.load
+    static fmload100  = 'dead/100-h/'+Keys.load
+    static fmloadherb = 'live/total herb/'+Keys.load
+    static fmloadstem = 'live/stem/'+Keys.load
+    static fmsavr1    = 'dead/1-h/'+Keys.savr
+    static fmsavrherb = 'live/herb/'+Keys.savr
+    static fmsavrstem = 'live/stem/'+Keys.savr
+    
     // Moisture Module properties
     static md1   = 'dead/1-h/' + Keys.mois
     static md10  = 'dead/10-h/' + Keys.mois
--- src/lib/modules/Units.js
+++ src/lib/modules/Units.js
@@ -1,10 +1,25 @@
 export class Units {
     static text = 'text'
     static cdeg = 'compass degrees from north'
-    
+    static ratio = 'ratio'
+    static fraction = 'fraction'
+    static factor = 'factor'
+    static length = 'length'                        // ft
+
+
+    // Fuel Module units
+    static depth = 'depth'
+    static diam = 'diameter'
+    static load = 'ovendry load'                    // lb/ft2
+    static dens = 'fiber density'                   // lb/ft3
+    static savr = 'surface area to volume ratio'    // ft2/ft3
+    static heat = 'heat of combustion'              // BTU/lb
+    static seff = 'ratio'
+    static stot = 'ratio'
+
     // Moisture Module units
     static mois = 'moisture content'
-    
+
     // Slope Module units
     static srat = 'vertical rise to horizontal reach'
     static sdeg = 'degrees above horizontal'
