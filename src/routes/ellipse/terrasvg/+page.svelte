e317778189667bbcfbce75e97ca30264c390c481
Collin D Bevins <cbevins@montana.com>
2025-12-22 23:11:29 -0700

    Updated Terra.js

--- src/routes/ellipse/terrasvg/+page.svelte
+++ src/routes/ellipse/terrasvg/+page.svelte
@@ -1,86 +1,36 @@
 <script>
-    import { onMount } from 'svelte'
     import { gxmlStr } from '$lib/gxml/gxmlStr'
-    import {Terra} from './terra.js'
+    import {Terra} from './Terra.js'
 
-    function label(easting, northing, str, stroke='black') {
-        const x = terra.x(easting)
-        const y = terra.y(northing)
-        els.push({el: 'text', x, y, stroke,
-            'text-anchor': 'middle', 'font-family': 'sans-serif',
-            'font-weight': 'light','font-size': 24,
-        els: [{el: 'inner', content: str}]})
-    }
-    function tic(easting, northing, str, anchor='middle', stroke='grey') {
-        const x = terra.x(easting)
-        const y = terra.y(northing)
-        els.push({el: 'text', x, y, stroke,
-            'text-anchor': anchor, 'font-family': 'sans-serif',
-            'font-weight': 'light','font-size': 12,
-        els: [{el: 'inner', content: str}]})
-    }
+    let {easting='1500', northing='8500', width='1000', height='1000', scale='1',
+        svgWidth='1000', svgHeight='1000'} = $props()
+    easting = parseFloat(easting)
+    northing = parseFloat(northing)
+    width = parseFloat(width)
+    height = parseFloat(height)
+    scale = parseFloat(scale)
+    svgHeight = parseFloat(svgHeight)
+    svgWidth = parseFloat(svgWidth)
 
-    // Define a viewport with a center point [easting, northing], a width and height,
-    // and a scale factor, all in map units (i.e., feet, meters, etc)
-    let easting = $state(1500)
-    let northing = $state(8500)
-    let width = $state(1000)
-    let height = $state(1000)
-    let scale = $state(1)
-    let svg = $state({el:'svg', width: 1000, height: 1000, els:[]})
-    let els = $state([])
+    let terra = $state(new Terra(easting, northing, scale, width, height,
+        svgWidth, svgHeight))
+    let svg = $state(terra.svg)
 
-    let terra = $state(new Terra(easting, northing, width, height, scale))
-
-    function grid(els, t) {
-        let x = t.x(t.easting)
-        els.push({el:'line', x1:x, y1: t.y(t.north), x2:x, y2: t.y(t.south), stroke: 'black'})
-        for(x=t.west; x<=t.east; x+=t.width/20) {
-            els.push({el:'line', x1: t.x(x), y1: t.y(t.north), x2: t.x(x), y2: t.y(t.south), stroke: 'grey'})
-            tic(x, t.north-5, x.toFixed(0))
-            tic(x, t.south+5, x.toFixed(0))
-        }
-        let y = t.y(t.northing)
-        els.push({el:'line', x1: t.x(t.west), y1:y, x2: t.x(t.east), y2:y, stroke: 'black'})
-        for(y=t.south; y<=t.north; y+=t.height/20) {
-            els.push({el:'line', x1: t.x(t.west), y1: t.y(y), x2: t.x(t.east), y2: t.y(y), stroke: 'grey'})
-            tic(t.west+5, y, y.toFixed(0), 'start')
-            tic(t.east-5, y, y.toFixed(0), 'end')
-        }
-    }
-    function directions() {
-        let fs = 24 / terra.scale // font size
-        label(terra.easting, terra.north-fs, 'N')
-        label(terra.east-fs, terra.northing, 'E')
-        label(terra.west+fs, terra.northing, 'W')
-        label(terra.easting, terra.south+fs, 'S')
-        label(terra.west+fs, terra.north-fs, 'NW')
-        label(terra.east-fs, terra.north-fs, 'NE')
-        label(terra.west+fs, terra.south+fs, 'SW')
-        label(terra.east-fs, terra.south+fs, 'SE')
-    }
     function scaleUp() {
-        scale = scale + 0.5
-        update()
+        scale = scale + 0.25
+        terra = terra.rescale(scale)
+        svg = terra.svg
     }
+
     function scaleDown() {
-        scale = Math.max(0.5, scale - 0.5)
-        update()
+        if (scale > 0.25) {
+            scale -= 0.25
+            terra = terra.rescale(scale)
+            svg = terra.svg
+        }
     }
-
-    function update() {
-        terra = new Terra(easting, northing, width, height, scale)
-        // Background field
-        els = []
-        els.push({el:'rect', x:0, y:0, width:terra.width, height:terra.height, stroke: 'black', fill:'green'})
-        grid(els, terra)
-        directions()
-        svg = {el:'svg', width: 1000, height: 1000, els:els}
-
-    }
-    // SVG
-	onMount( () => { update() })
 </script>
+
 <div class='border mx-4 my-4 px-4 py-4'>
     <div class='ml-4 mt-2 mb-2'>
         <button onclick={scaleUp}  type="button"
@@ -94,7 +44,7 @@
     </div>
 
     <div>
-        Scale={terra.scale}, Width={terra.width}, Height={terra.height}
+        Scale={scale}, Width={width}, Height={height}
     </div>
     <div>
         West={terra.west}, East={terra.east}, North={terra.north}, South={terra.south}
--- src/routes/ellipse/terrasvg/terra.js
+++ src/routes/ellipse/terrasvg/terra.js
@@ -1,19 +1,115 @@
 export class Terra {
-    constructor(easting, northing, width=1000, height=1000, scale=1) {
-        this.width = width
-        this.height = height
+    constructor(easting, northing, scale=1, width=1000, height=1000, 
+        svgWidth=1000, svgHeight=1000) {
+        this.width = width          // field width in user units when scale = 1
+        this.height = height        // field height in user units when scale = 1
+        this.easting = easting      // field center easting (x) in user units
+        this.northing = northing    // field center northing (y) in user units
+        this.scale = scale          // SVG pixel-to-user units
+        this.svgWidth = svgWidth    // SVG width
+        this.svgHeight = svgHeight  // SVG height
+        this.els = []
+        this.svg = {el:'svg', width: this.svgWidth, height: this.svgHeight, els: this.els}
+        this._update()
+    }
+
+    recenter(easting, northing) {
         this.easting = easting
         this.northing = northing
-        this.scale = scale
-        this.updateBoundaries()
+        this._update()
+        return this
     }
-    updateBoundaries() {
+
+    rescale(scale) {
+        this.scale = scale
+        this._update()
+        return this
+    }
+
+    // Returns x and/or y in SVG coordinates
+    x(easting) { return this.scale * (easting - this.west) }
+    y(northing) { return this.scale * (this.north - northing) }
+    xy(easting, northing) { return [this.x(easting), this.y(northing)]}
+
+    //--------------------------------------------------------------------------
+    // Private methods
+    //--------------------------------------------------------------------------
+
+    _update() {
+        this._setBounds()
+        this._setSvg()
+    }
+
+    _setBounds() {
         this.north = this.northing + this.scale * this.height/2
         this.south = this.northing - this.scale * this.height/2
         this.west = this.easting - this.scale * this.width/2
         this.east = this.easting + this.scale * this.width/2
     }
-    x(easting) { return this.scale*(easting - this.west) }
-    y(northing) { return this.scale*(this.north - northing) }
-    xy(easting, northing) { return [this.x(easting), this.y(northing)]}
+
+    _setSvg() {
+        this.els = []
+        this.els.push({el:'rect', x:0, y:0, width:this.width, height:this.height,
+            stroke: 'black', fill:'green'})
+        this._addGrid()
+        this._addCompass()
+        this.svg = {el:'svg', width: this.svgWidth, height: this.svgHeight, els: this.els}
+    }
+
+    _addCompass() {
+        let fs = 24 / this.scale // font size
+        this._addLabel(this.easting, this.north-fs, 'N')
+        this._addLabel(this.east-fs, this.northing, 'E')
+        this._addLabel(this.west+fs, this.northing, 'W')
+        this._addLabel(this.easting, this.south+fs, 'S')
+        this._addLabel(this.west+fs, this.north-fs, 'NW')
+        this._addLabel(this.east-fs, this.north-fs, 'NE')
+        this._addLabel(this.west+fs, this.south+fs, 'SW')
+        this._addLabel(this.east-fs, this.south+fs, 'SE')
+    }
+
+    _addGrid() {
+        let x = this.x(this.easting)
+        this.els.push({el:'line', stroke: 'black',
+            x1:x, y1: this.y(this.north),
+            x2:x, y2: this.y(this.south)})
+        for(x=this.west; x<=this.east; x+=this.width/20) {
+            this.els.push({el:'line', stroke: 'grey',
+                x1: this.x(x), y1: this.y(this.north),
+                x2: this.x(x), y2: this.y(this.south)})
+            this._addTic(x, this.north-5, x.toFixed(0))
+            this._addTic(x, this.south+5, x.toFixed(0))
+        }
+        let y = this.y(this.northing)
+        this.els.push({el:'line', stroke: 'black',
+            x1: this.x(this.west), y1:y,
+            x2: this.x(this.east), y2:y})
+        for(y=this.south; y<=this.north; y+=this.height/20) {
+            this.els.push({el:'line', stroke: 'grey',
+                x1: this.x(this.west), y1: this.y(y),
+                x2: this.x(this.east), y2: this.y(y)})
+            this._addTic(this.west+5, y, y.toFixed(0), 'start')
+            this._addTic(this.east-5, y, y.toFixed(0), 'end')
+        }
+    }
+
+    _addLabel(easting, northing, str, stroke='black') {
+        const x = this.x(easting)
+        const y = this.y(northing)
+        this.els.push({el: 'text', x, y, stroke,
+            'text-anchor': 'middle', 'font-family': 'sans-serif',
+            'font-weight': 'light','font-size': 24,
+            els: [{el: 'inner', content: str}]
+        })
+    }
+
+    _addTic(easting, northing, str, anchor='middle', stroke='grey') {
+        const x = this.x(easting)
+        const y = this.y(northing)
+        this.els.push({el: 'text', x, y, stroke,
+            'text-anchor': anchor, 'font-family': 'sans-serif',
+            'font-weight': 'light','font-size': 12,
+            els: [{el: 'inner', content: str}]
+        })
+    }
 }
